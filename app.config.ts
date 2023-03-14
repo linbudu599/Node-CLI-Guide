600995eb9aa1c81a2b2ca288e87f5861c6a41efa
linbudu <linbudu599@gmail.com>
2023-03-14 21:02:28 +0800

    feat: setup

---
+++ app.config.ts
@@ -0,0 +1,27 @@
+export default defineAppConfig({
+  docus: {
+    title: 'NodeJS 命令行开发教程',
+    url: 'https://github.com/linbudu599/Node-CLI-Guide',
+    image: '/logo.png',
+    github: {
+      root: 'content',
+      edit: true,
+      contributors: true,
+      owner: 'linbudu599',
+      repo: 'https://github.com/linbudu599/Node-CLI-Guide',
+      branch: 'main',
+    },
+    header: {
+      title: 'LinbuduLab',
+      logo: false,
+      showLinkIcon: true,
+    },
+    footer: {
+      credits: {
+        icon: 'IconDocus',
+        text: 'Powered by Docus',
+        href: 'https://docus.com',
+      },
+    },
+  },
+});
---
+++ content/index.md
@@ -0,0 +1,34 @@
+---
+title: NodeJs 命令行开发
+navigation: false
+layout: page
+---
+
+:ellipsis{right=0px width=75% blur=150px}
+
+::block-hero
+---
+
+cta:
+  - Get Started →
+  - /intro
+secondary:
+  - View GitHub →
+  - https://github.com/linbudu599/Node-CLI-Guide
+---
+
+#title
+NodeJs 命令行开发
+
+#description
+免费，开源，跟得上时代的命令行开发教程
+
+#extra
+  ::list
+  - 实战与原理解析
+  - 一网打尽命令行交互：Loading、Prompt、Progress...
+  - 为命令行应用编写测试
+  - 从开发到发布的全流程介绍
+  ::
+
+::
